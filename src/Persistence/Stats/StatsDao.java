57ab40703c7bf6982b212ded5b56b3d91586c518
Walter Quintili <150900010+wquintili88@users.noreply.github.com>
2025-02-16 17:51:50 +0100

    update stats api && json

--- src/Persistence/Stats/StatsDao.java
+++ src/Persistence/Stats/StatsDao.java
@@ -1,9 +1,11 @@
 package src.Persistence.Stats;
 
 import edu.salle.url.api.exception.ApiException;
+import src.Bussines.Stats;
 import src.Bussines.Team;
 import java.util.List;
 
 public interface StatsDao {
     List<Team> readStats() throws ApiException;
+    void updateStats(Stats team1, Stats team2) throws ApiException;
 }
